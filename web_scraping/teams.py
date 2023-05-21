7dc65a0912dcfae73432af1e7f9ec9f779d81630
powerostad <90046706+Powerostad@users.noreply.github.com>
2023-05-21 22:19:51 +0330

    change cups data

--- web_scraping/teams.py
+++ web_scraping/teams.py
@@ -23,7 +23,11 @@ for league in leagues :
             cups_num = soup.select('.data-header__success-number')
             cups_list = [cup.get('title') for cup in cups]
             cups_num_list = [num.text for num in cups_num]
-            data_team['cups'] = [{'cup_name':value,'cup_num':num_value} for value,num_value in zip(cups_list,cups_num_list)]
+            data_team['cups'] = [[f'{value}:{num_value}'] for value,num_value in zip(cups_list,cups_num_list)]
+            data_team['national_team_players'] = soup.select('.data-header__label:nth-child(1) a')[0].text
+            data_team['market_value'] = soup.select('.data-header__market-value-wrapper')[0].text
+            data_team['average_age'] = soup.select('.data-header__items:nth-child(1) .data-header__label:nth-child(2) .data-header__content')[0].text
+            
 
 
 
