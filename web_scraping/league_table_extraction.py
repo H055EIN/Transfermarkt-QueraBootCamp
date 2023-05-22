7461d72410fa7dd8b3489293066d4872d8734d9a
Fateme <fatemehmohammadiva@gmail.com>
2023-05-22 13:15:44 +0330

    Added league,season,team_id,rank and all tables have been merged.

--- web_scraping/league_table_extraction.py
+++ web_scraping/league_table_extraction.py
@@ -1,46 +1,93 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-leagues = [
-    ("https://www.transfermarkt.us/premier-league/tabelle/wettbewerb/GB1?saison_id=", "Premier League"),
-    ("https://www.transfermarkt.us/serie-a/tabelle/wettbewerb/IT1?saison_id=", "Serie A"),
-    ("https://www.transfermarkt.us/laliga/tabelle/wettbewerb/ES1?saison_id=", "La Liga"),
-    ("https://www.transfermarkt.us/bundesliga/tabelle/wettbewerb/L1?saison_id=", "Bundesliga"),
-    ("https://www.transfermarkt.us/ligue-1/tabelle/wettbewerb/FR1?saison_id=", "Ligue 1")
-]
-seasons = range(2015, 2022)
-max_retries = 3
+# Set the display options to show all columns
+pd.set_option('display.max_columns', None)
 
-for season in seasons:
-    for url, league in leagues:
-        full_url = f"{url}{season}"
-        retries = 0
-        success = False
 
-        while retries < max_retries and not success:
-            try:
-                html_content = requests.get(full_url, headers={'User-Agent': 'Mozilla/5.0',
-                                                               "Accept-Language": "en-US,en;q=0.5"}).text
-                success = True
-            except requests.exceptions.RequestException:
-                print(f"Error occurred while making a request. Retrying in 5 seconds...")
-                time.sleep(5)
-                retries += 1
+def get_league_data():
+    # Define the leagues and their respective URLs
+    leagues = [
+        ("https://www.transfermarkt.us/premier-league/tabelle/wettbewerb/GB1?saison_id=", "Premier League"),
+        ("https://www.transfermarkt.us/serie-a/tabelle/wettbewerb/IT1?saison_id=", "Serie A"),
+        ("https://www.transfermarkt.us/laliga/tabelle/wettbewerb/ES1?saison_id=", "La Liga"),
+        ("https://www.transfermarkt.us/bundesliga/tabelle/wettbewerb/L1?saison_id=", "Bundesliga"),
+        ("https://www.transfermarkt.us/ligue-1/tabelle/wettbewerb/FR1?saison_id=", "Ligue 1")
+    ]
 
-        if not success:
-            print(f"Failed to retrieve data from {full_url}. Skipping...")
-            continue
+    # Define the range of seasons
+    seasons = range(2015, 2022)
 
-        soup = BeautifulSoup(html_content, "html.parser")
-        table = soup.find_all("table")[1]
-        headers = [th.text.strip() for th in table.find_all("th")]
-        headers[2] = 'Matches'
-        rows = []
-        for tr in table.find_all("tr")[1:]:
-            cells = [td.text.strip() for td in tr.find_all("td")]
-            rows.append(cells[1:])  # Exclude the first column (position number)
-        df = pd.DataFrame(rows, columns=headers)
-        print(df)
-        print("\n\n")
+    # Maximum number of retries for making requests
+    max_retries = 3
+
+    all_tables = []  # List to store all the tables
+
+    for season in seasons:
+        for url, league_name in leagues:
+            full_url = f"{url}{season}"
+            retries = 0
+            success = False
+
+            # Retry the request until success or maximum retries reached
+            while retries < max_retries and not success:
+                try:
+                    html_content = requests.get(full_url, headers={'User-Agent': 'Mozilla/5.0',
+                                                                   "Accept-Language": "en-US,en;q=0.5"}).text
+                    success = True
+                except requests.exceptions.RequestException:
+                    print(f"Error occurred while making a request. Retrying in 5 seconds...")
+                    time.sleep(5)
+                    retries += 1
+
+            if not success:
+                print(f"Failed to retrieve data from {full_url}. Skipping...")
+                continue
+
+            soup = BeautifulSoup(html_content, "html.parser")
+            league = soup.find('h1').text.strip()
+
+            # Find the table containing the data
+            table = soup.find_all("table")[1]
+
+            # Extract table headers
+            headers = [th.text.strip() for th in table.find_all("th")]
+            headers.insert(1, '')
+            headers[3] = 'Matches'
+            headers[0] = 'Rank'
+
+            rows = []
+            team_ids = []
+
+            # Extract data rows from the table
+            for tr in table.find_all("tr")[1:]:
+                cells = [td.text.strip() for td in tr.find_all("td")]
+                rows.append(cells)
+
+                # Extract team ID from the club URL
+                club_url = tr.find("td", class_='zentriert').find("a").get('href')
+                pattern = r"\/verein\/(\d+)\/saison_id\/\d+"
+                match = re.search(pattern, club_url)
+                if match:
+                    team_id = int(match.group(1))
+                    team_ids.append(team_id)
+
+            # Create a DataFrame from the extracted data
+            df = pd.DataFrame(rows, columns=headers)
+            # df = df.drop('', axis=1) # managing conflict
+            df.insert(2, "Season", f'{season}/{season + 1}')
+            df.insert(1, "League", league)
+            df.insert(3, "club_id", team_ids)
+
+            all_tables.append(df)  # Append the current table to the list
+
+    # Concatenate all the tables into a single DataFrame
+    merged_df = pd.concat(all_tables, ignore_index=True)
+
+    return merged_df
+
+
+get_league_data()
