3f0711d5d95eb493debe4d206b1518be0fbbef50
Fateme <fatemehmohammadiva@gmail.com>
2023-05-26 15:43:50 +0330

    inserting

--- database/insert.py
+++ database/insert.py
@@ -1,6 +1,7 @@
 import json
 from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 from model import Team
 
 DB_NAME = 'Transfermarkt_database'
@@ -11,7 +12,6 @@ url_object = URL.create(
     password="suramii78",
     host="localhost",
     database=DB_NAME
-
 )
 
 engine = create_engine(url_object)
@@ -21,7 +21,28 @@ session = Session()
 with open('..\\web_scraping\\teams.json', 'r') as f:
     data = json.load(f)
 
-teams = [Team(id=t['id'], team_name=t['team_name'], market_value=t['market_value'], average_age=t['average_age'],)
-         for t in data]
+for entry in data:
+    team_id = entry["id"]
+    query = select(Team).filter_by(id=team_id)
+    existing_team = session.scalars(query).first()
 
-session.add_all(teams)
+    if existing_team is None:
+        average_age = entry["average_age"].strip()
+        market_value = entry["market_value"]
+        if isinstance(market_value, str):
+            market_value = market_value.replace('€', '').replace('m', '')
+        else:
+            market_value = None
+
+        team = Team(
+            id=team_id,
+            team_name=entry["team_name"],
+            market_value=market_value,
+            average_age=average_age
+        )
+
+        # Add the team to the session
+        session.add(team)
+
+# Commit the changes to the database
+session.commit()
