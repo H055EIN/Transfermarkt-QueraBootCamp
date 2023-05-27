a612d36523317a0b6b1516ae1e6ecf3a1e451b90
powerostad <90046706+Powerostad@users.noreply.github.com>
2023-05-27 19:05:26 +0330

    database added

--- database/insert.py
+++ database/insert.py
@@ -1,15 +1,17 @@
 import json
+import os
+import numpy as np
 from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
-from model import Team,Competition
+from model import Team,Season,Competition,Achievement,Transfer,PlayerStat,Player
 
 DB_NAME = 'Transfermarkt_database'
 
 url_object = URL.create(
     "mysql+mysqlconnector",
     username="root",
-    password="suramii78",
+    password="1234",
     host="localhost",
     database=DB_NAME
 )
@@ -18,33 +20,187 @@ engine = create_engine(url_object)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open('..\\web_scraping\\teams.json') as f:
-    data = json.load(f)
-unique_team_ids = set()
-for entry in data:
-    team_id = entry["id"]
-    query = select(Team).filter_by(id=team_id)
-    existing_team = session.scalars(query).first()
-
-    if existing_team is None:
-        average_age = entry["average_age"].strip()
-        market_value = entry["market_value"]
-        if isinstance(market_value, str):
-            market_value = market_value.replace('€', '').replace('m', '')
-        else:
-            market_value = None
-
-        team = Team(
-            id=team_id,
-            team_name=entry["team_name"],
-            market_value=market_value,
-            average_age=average_age
-        )
-
+# adding Season
+for i in range(15,23):
+    sseason = Season(
+        start_at = i,
         
+    )
+    session.add(sseason)
 
-        # Add the team to the session
-        session.add(team)
+
+# adding Team and Achievement
+os.chdir('./web_scraping')
+with open('teams.json' , "r") as f:
+    data_team = json.load(f)
+
+extra_team = Team(
+    id=0,
+    team_name="Others",
+    market_value=None,
+)
+session.add(extra_team)
+unique_team_ids = set()
+for entry in data_team:
+    team_id = entry["id"]
+    if team_id in unique_team_ids:
+        continue
+    unique_team_ids.add(team_id)
+    # try:
+    #     average_age = float(entry["average_age"].strip())
+    # except ValueError:
+    #     print(repr(entry["average_age"]))
+    #     print(team_id)
+    #     exit(1)
+    market_value = entry["market_value"]
+
+    if isinstance(market_value, str):
+        if market_value.endswith('m'):
+            market_value = float(market_value[1:-1].strip())
+        else:
+            market_value = float(market_value[1:-2].strip())
+    else:
+        market_value = None
+
+    steam = Team(
+        id=team_id,
+        team_name=entry["team_name"],
+        market_value=market_value,
+        # average_age=average_age
+    )
+    session.add(steam)    
+
+    for cup in entry["cups"] : 
+        sachievement = Achievement(
+            cup_name = cup[0],
+            cup_count = cup[1],
+            team_id = steam.id
+        )
+        session.add(sachievement)
+
+
+# add Competition
+with open('P2_competitions.json') as f2:
+    data_competition = json.load(f2)
+
+
+    for entry2 in data_competition:
+        scompetition = Competition(
+        competition_name = entry2["Competition"]
+        )
+        session.add(scompetition)
+
+# add Player
+with open('Players.json') as f3:
+    data_player = json.load(f3)
+
+def to_minute(string):
+    if string:
+        if len(string) >4 :
+            return int(string.replace("'","").replace(".",""))
+        else:
+            return int(string.replace("'",""))
+    else:
+        return None
+
+def correct_value(string):
+    if string != "-":
+        if string[-1] == "m":
+            return float(string.replace("m","")) * 1000000
+        else:
+            return float(string.replace("k","")) * 1000
+    else:
+        return None
+
+for player in data_player:
+    splayer = Player(
+        id = player["id"],
+        current_team = player["Current club"],
+        full_name = player["full_name"],
+        age = int(player["age"]) if player["age"] else None,
+        birth_place = player["birth_place"],
+        height = float(player["height"].replace(",","").replace(" ","").replace("m","")) if (player["height"]) and (player["height"] != 'N/A') else None,
+        citizenship = ",".join(player["citizenship"]),
+        nationality = player['nationality'],
+        main_position = player["main_position"],
+        other_position = ",".join(player['available_positions']),
+        foot = player["foot"],
+        total_goals_in_clubs = int(player["total_goals_in_clubs"]) if player["total_goals_in_clubs"] != "-" else 0,
+        total_assists = int(player["total_assists"]) if (player["total_assists"] != "-") and player["total_assists"] else None,
+        international_goals = int(player["international_goals"]) if player["international_goals"] else None,
+        caps = int(player["caps"]) if player["caps"] else None,
+        total_squad = player["total_squad"],
+        total_appearance = int(player["total_apperance"]) if player["total_goals_in_clubs"] != "-" else None,
+        total_own_goal = int(player["total_own_goal"]) if player["total_own_goal"] != "-" else 0,
+        total_sub_off = int(player["total_sub_off"]) if player["total_sub_off"] != "-" else 0,
+        total_sub_on = int(player["total_sub_on"]) if player["total_sub_on"] != "-" else 0,
+        total_yellow_card = int(player["total_yellow_card"]) if player["total_yellow_card"] != "-" else 0,
+        total_second_yellow_card = int(player["total_second_yellow_card"]) if player["total_second_yellow_card"] != "-" else 0,
+        total_red_card = int(player["total_red_card"]) if player["total_red_card"] != "-" else 0,
+        total_penalty = int(player["total_penalty"]) if (player["total_penalty"] != "-") and player["total_penalty"] else 0,
+        total_minutes_per_goal = to_minute(player["total_minutes_per_goal"]) if player["total_minutes_per_goal"] != "-" else 0,
+        total_minutes_play = to_minute(player["total_minutes_per_goal"]) if player["total_minutes_per_goal"] != "-" else 0,
+        total_goal_conceded = int(player["total_goal_conceded"]) if (player["total_goal_conceded"]) and (player["total_goal_conceded"] != "-") else None,
+        total_clean_sheet = int(player["total_clean_sheets"]) if (player["total_clean_sheets"]) and (player["total_clean_sheets"] != "-") else None,
+        total_PPG = float(player["total_ppg"]) if (player["total_ppg"] != "-") and (player["total_ppg"] != '') else None,
+        highest_market_value = correct_value(player["highest_market_value"]) if player["highest_market_value"] else None,
+        current_market_value = correct_value(player["current_market_value"]) if player["current_market_value"] else None,
+
+    ) 
+    session.add(splayer)
+
+# add Player stat
+
+def ppg_fix(string):
+    if string == "-":
+        return None
+    elif len(string)>1 and string[1] == ",":
+        return float(string.replace(",","."))
+    else:
+        return float(string)
+
+with open('P2_player_state.json') as f4:
+    data_playerstat = json.load(f4)
+for data in data_playerstat:
+    q1 = select(Player).filter_by(id=data["Player_id"])
+    q_player_id = session.scalars(q1).first()
+
+    
+    q_competition_id = session.query(Competition).filter_by(competition_name=data["Competition"]).first()
+    
+    q_season_id = session.query(Season).filter_by(start_at=int(data["Season"][0:2])).first()
+    q_team_id = session.query(Team).filter_by(id=data["Team_id"]).first()
+    
+    if q_team_id is None:
+        q_team_id = session.query(Team).filter_by(team_name="Others").first()
+    if int(data["Season"][0:2]) in [15,16,17,18,19,20,21]:
+        splayer_stat = PlayerStat(
+            player_id = q_player_id.id,
+            competition_id = q_competition_id.id,
+        
+            team_id = q_team_id.id,
+            season_id = q_season_id.id,
+            squad = data["Squad"],
+            appearance = int(data["Apperance"]) if data["Apperance"] != "-" else None,
+            PPG = ppg_fix(data['PPG']),
+            goals = int(data["Goals"]) if data["Goals"] != "-" else None,
+            assists = int(data["Assists"]) if (data["Assists"] != "-") and(data["Assists"]) else None,
+            own_goal = int(data["Own_goal"]) if data["Own_goal"] != "-" else None,
+            sub_off = int(data["Substitutions_off"]) if data["Substitutions_off"] != "-" else None,
+            sub_on = int(data["Substitutions_on"]) if data["Substitutions_on"] != "-" else None,
+            yellow_card = int(data["Yellow_card"]) if data["Yellow_card"] != "-" else None,
+            second_yellow_card = int(data["Second_yellow_card"]) if data["Second_yellow_card"] != "-" else None,
+            red_card = int(data["Red_card"]) if data["Red_card"] != "-" else None,
+            penalty_goals = int(data["Penalty_goals"]) if (data["Penalty_goals"] != "-") and (data["Penalty_goals"]) else None,
+            clean_sheets =int(data["Clean_sheets"]) if (data["Clean_sheets"] != "-") and (data["Clean_sheets"]) else None,
+            goal_conceded =int(data["Goals_conceded"]) if (data["Goals_conceded"] != "-") and (data["Goals_conceded"]) else None,
+            minutes_per_goal = to_minute(data["Minutes_per_goal"]) if data["Minutes_per_goal"] != "-" else None,
+            minutes_play = to_minute(data["Minutes_played"]) if data["Minutes_played"] != "-" else None,
+        )
+        session.add(splayer_stat)   
+
+
+
 
 # Commit the changes to the database
 session.commit()
--- database/model.py
+++ database/model.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List,Optional
 
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy import URL
@@ -54,8 +54,8 @@ class Team(Base):
     market_value: Mapped[float] = mapped_column(Float, nullable=True)
     # average_age: Mapped[float] = mapped_column(Float, nullable=True)
 
-    player_stat: Mapped["PlayerStat"] = relationship('PlayerStat', back_populates="team")
-    teamstats: Mapped["TeamStat"] = relationship('TeamStat', back_populates="team")
+    players_stats: Mapped["PlayerStat"] = relationship('PlayerStat', back_populates="team")
+    teams_stats: Mapped["TeamStat"] = relationship('TeamStat', back_populates="team")
     achievements: Mapped["Achievement"] = relationship('Achievement', back_populates="team")
 
 
@@ -68,10 +68,9 @@ class Season(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     start_at: Mapped[int] = mapped_column(Integer)
-    end_at: Mapped[int] = mapped_column(Integer)
 
-    player_stat: Mapped[List["PlayerStat"]] = relationship(back_populates="season")
-    teamstats: Mapped[List["TeamStat"]] = relationship(back_populates="season")
+    players_stats: Mapped[List["PlayerStat"]] = relationship(back_populates="season")
+    teams_stats: Mapped[List["TeamStat"]] = relationship(back_populates="season")
     transfers: Mapped[List["Transfer"]] = relationship(back_populates="season")
 
 
@@ -80,9 +79,8 @@ class Competition(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     competition_name: Mapped[str] = mapped_column(String(128))
-    league_id: Mapped[int] = mapped_column(Integer)
 
-    player_stat: Mapped[List["PlayerStat"]] = relationship(back_populates="competition")
+    players_stats: Mapped[List["PlayerStat"]] = relationship(back_populates="competition")
 
     def __repr__(self):
         return f"Season(id={self.id}, name='{self.name}', start_at={self.start_at}, end_at={self.end_at})"
@@ -107,10 +105,10 @@ class Transfer(Base):
     player_id: Mapped[int] = mapped_column(ForeignKey("player.id"))
     origin_team_id: Mapped[int] = mapped_column(Integer)
     destination_team_id: Mapped[int] = mapped_column(Integer)
-    mv: Mapped[float] = mapped_column(Float)
-    fee: Mapped[float] = mapped_column(Float)
-    joined: Mapped[str] = mapped_column(String(64))
-    left: Mapped[str] = mapped_column(String(64))
+    mv: Mapped[Optional[float]] = mapped_column(Float,nullable=True)
+    fee: Mapped[Optional[str]] = mapped_column(String(64),nullable=True)
+    joined: Mapped[str] = mapped_column(String(128))
+    left: Mapped[str] = mapped_column(String(128))
 
     player: Mapped["Player"] = relationship(back_populates="transfers")
     season: Mapped["Season"] = relationship(back_populates="transfers")
@@ -120,38 +118,38 @@ class Player(Base):
     __tablename__ = "player"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    current_team: Mapped[str] = mapped_column(String(128))
-    full_name: Mapped[str] = mapped_column(String(128))
-    age: Mapped[int] = mapped_column(Integer)
-    birth_place: Mapped[str] = mapped_column(String(128))
-    height: Mapped[float] = mapped_column(Float)
-    citizenship: Mapped[List[str]] = mapped_column(String(128))
-    nationality: Mapped[str] = mapped_column(String(64))
-    main_position: Mapped[str] = mapped_column(String(64))
-    other_position: Mapped[List[str]] = mapped_column(String(128))
-    foot: Mapped[str] = mapped_column(String(32))
-    total_goals_in_clubs: Mapped[int] = mapped_column(Integer)
-    total_assists: Mapped[int] = mapped_column(Integer)
-    international_goals: Mapped[int] = mapped_column(Integer)
-    caps: Mapped[int] = mapped_column(Integer)
-    total_squad: Mapped[int] = mapped_column(Integer)
-    total_appearance: Mapped[int] = mapped_column(Integer)
-    total_own_goal: Mapped[int] = mapped_column(Integer)
-    total_sub_off: Mapped[int] = mapped_column(Integer)
-    total_sub_on: Mapped[int] = mapped_column(Integer)
-    total_yellow_card: Mapped[int] = mapped_column(Integer)
-    total_second_yellow_card: Mapped[int] = mapped_column(Integer)
-    total_red_card: Mapped[int] = mapped_column(Integer)
-    total_penalty: Mapped[int] = mapped_column(Integer)
-    total_minutes_per_goal: Mapped[float] = mapped_column(Float)
-    total_minutes_play: Mapped[float] = mapped_column(Float)
-    total_goal_conceded: Mapped[int] = mapped_column(Integer)
-    total_clean_sheet: Mapped[int] = mapped_column(Integer)
-    total_PPG: Mapped[float] = mapped_column(Float)
-    highest_market_value: Mapped[float] = mapped_column(Float)
-    current_market_value: Mapped[float] = mapped_column(Float)
+    current_team: Mapped[str] = mapped_column(String(128),nullable=True)
+    full_name: Mapped[str] = mapped_column(String(128),nullable=True)
+    age: Mapped[int] = mapped_column(Integer,nullable=True)
+    birth_place: Mapped[str] = mapped_column(String(128),nullable=True)
+    height: Mapped[float] = mapped_column(Float,nullable=True)
+    citizenship: Mapped[str] = mapped_column(String(255),nullable=True)
+    nationality: Mapped[str] = mapped_column(String(64),nullable=True)
+    main_position: Mapped[str] = mapped_column(String(64),nullable=True)
+    other_position: Mapped[str] = mapped_column(String(255),nullable=True)
+    foot: Mapped[str] = mapped_column(String(32),nullable=True)
+    total_goals_in_clubs: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_assists: Mapped[int] = mapped_column(Integer,nullable=True)
+    international_goals: Mapped[int] = mapped_column(Integer,nullable=True)
+    caps: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_squad: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_appearance: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_own_goal: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_sub_off: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_sub_on: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_yellow_card: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_second_yellow_card: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_red_card: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_penalty: Mapped[int] = mapped_column(Integer,nullable=True)
+    total_minutes_per_goal: Mapped[float] = mapped_column(Float,nullable=True)
+    total_minutes_play: Mapped[float] = mapped_column(Float,nullable=True)
+    total_goal_conceded: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    total_clean_sheet: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    total_PPG: Mapped[float] = mapped_column(Float,nullable=True)
+    highest_market_value: Mapped[float] = mapped_column(Float,nullable=True)
+    current_market_value: Mapped[float] = mapped_column(Float,nullable=True)
 
-    player_stat: Mapped[List["PlayerStat"]] = relationship('PlayerStat', back_populates="player")
+    players_stats: Mapped[List["PlayerStat"]] = relationship('PlayerStat', back_populates="player")
     transfers: Mapped[List["Transfer"]] = relationship(back_populates="player")
 
 
@@ -162,28 +160,28 @@ class PlayerStat(Base):
     player_id: Mapped[int] = mapped_column(ForeignKey("player.id"))
     competition_id: Mapped[int] = mapped_column(ForeignKey("competition.id"))
     team_id: Mapped[int] = mapped_column(ForeignKey("team.id"))
-    squad: Mapped[int] = mapped_column(Integer)
-    appearance: Mapped[int] = mapped_column(Integer)
-    PPG: Mapped[float] = mapped_column(Float)
-    goals: Mapped[int] = mapped_column(Integer)
-    assists: Mapped[int] = mapped_column(Integer)
+    squad: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    appearance: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    PPG: Mapped[Optional[float]] = mapped_column(Float,nullable=True)
+    goals: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    assists: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
     season_id: Mapped[int] = mapped_column(ForeignKey("season.id"))
-    own_goal: Mapped[int] = mapped_column(Integer)
-    sub_off: Mapped[int] = mapped_column(Integer)
-    sub_on: Mapped[int] = mapped_column(Integer)
-    yellow_card: Mapped[int] = mapped_column(Integer)
-    second_yellow_card: Mapped[int] = mapped_column(Integer)
-    red_card: Mapped[int] = mapped_column(Integer)
-    penalty_goals: Mapped[int] = mapped_column(Integer)
-    clean_sheets: Mapped[int] = mapped_column(Integer)
-    goal_conceded: Mapped[int] = mapped_column(Integer)
-    minutes_per_goal: Mapped[int] = mapped_column(Integer)
-    minutes_play: Mapped[int] = mapped_column(Integer)
+    own_goal: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    sub_off: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    sub_on: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    yellow_card: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    second_yellow_card: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    red_card: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    penalty_goals: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    clean_sheets: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    goal_conceded: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    minutes_per_goal: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
+    minutes_play: Mapped[Optional[int]] = mapped_column(Integer,nullable=True)
 
-    team: Mapped["Team"] = relationship(back_populates="player_stat")
-    player: Mapped["Player"] = relationship(back_populates="player_stat")
-    competition: Mapped["Competition"] = relationship(back_populates="player_stat")
-    season: Mapped["Season"] = relationship(back_populates="player_stat")
+    team: Mapped["Team"] = relationship(back_populates="players_stats")
+    player: Mapped["Player"] = relationship(back_populates="players_stats")
+    competition: Mapped["Competition"] = relationship(back_populates="players_stats")
+    season: Mapped["Season"] = relationship(back_populates="players_stats")
 
 
 class TeamStat(Base):
@@ -192,16 +190,18 @@ class TeamStat(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     team_id: Mapped[int] = mapped_column(ForeignKey("team.id"))
     season_id: Mapped[int] = mapped_column(ForeignKey("season.id"))
+    league:Mapped[str] = mapped_column(String(64))
     matches: Mapped[int] = mapped_column(Integer)
     wins: Mapped[int] = mapped_column(Integer)
     losts: Mapped[int] = mapped_column(Integer)
     draws: Mapped[int] = mapped_column(Integer)
-    goals: Mapped[int] = mapped_column(Integer)
+    goals: Mapped[str] = mapped_column(String(32))
     goal_differenece: Mapped[int] = mapped_column(Integer)
     pts: Mapped[int] = mapped_column(Integer)
-
-    team: Mapped["Team"] = relationship(back_populates="teamstats")
-    season: Mapped["Season"] = relationship(back_populates="teamstats")
+    rank = Mapped[int] = mapped_column(Integer)
+    
+    team: Mapped["Team"] = relationship(back_populates="teams_stats")
+    season: Mapped["Season"] = relationship(back_populates="teams_stats")
 
 
 Base.metadata.create_all(engine)
